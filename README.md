16232a4753d882e6942888fe639f329ba8f8c690
bopsec <bop@bopsec.com>
2026-06-25 11:48:49 +0200

    Remove potion storage initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+# Remove potion storage
+Removes the potion storage icon from bank if you regret your purchase
+
 ## Support/Contact me
 If you have any suggestions or need support, you can open a Github issue, or contact me [in my discord server.](https://discord.gg/AHcvPd9uru)
---
+++ src/main/java/com/bop/RemovePotionStoragePlugin.java
@@ -0,0 +1,82 @@
+package com.bop;
+
+import javax.inject.Inject;
+import net.runelite.api.Client;
+import net.runelite.api.events.WidgetLoaded;
+import net.runelite.api.gameval.InterfaceID;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+	name = "Remove potion storage",
+	description = "Removes the potion storage icon in bank"
+)
+public class RemovePotionStoragePlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
+
+	private Boolean storagePopupTabWasHidden;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		clientThread.invoke(this::hideStoragePopupTab);
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		clientThread.invoke(this::restoreStoragePopupTab);
+	}
+
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded widgetLoaded)
+	{
+		if (widgetLoaded.getGroupId() == InterfaceID.BANKMAIN)
+		{
+			clientThread.invokeLater(this::hideStoragePopupTab);
+		}
+	}
+
+	private void hideStoragePopupTab()
+	{
+		Widget storagePopupTab = client.getWidget(InterfaceID.Bankmain.STORAGE_POPUP_TAB);
+		if (storagePopupTab == null)
+		{
+			return;
+		}
+
+		if (storagePopupTabWasHidden == null)
+		{
+			storagePopupTabWasHidden = storagePopupTab.isSelfHidden();
+		}
+
+		storagePopupTab.setHidden(true);
+		storagePopupTab.revalidate();
+
+		Widget parent = storagePopupTab.getParent();
+		if (parent != null)
+		{
+			parent.revalidate();
+		}
+	}
+
+	private void restoreStoragePopupTab()
+	{
+		Widget storagePopupTab = client.getWidget(InterfaceID.BANKMAIN, InterfaceID.Bankmain.STORAGE_POPUP_TAB);
+		if (storagePopupTab != null && storagePopupTabWasHidden != null)
+		{
+			storagePopupTab.setHidden(storagePopupTabWasHidden);
+			storagePopupTab.revalidate();
+		}
+
+		storagePopupTabWasHidden = null;
+	}
+}
