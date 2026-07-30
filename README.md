b2d16e6c4eaad12c6e5f27684152a9d466efe77a
bopsec <bop@bopsec.com>
2026-07-30 02:06:07 +0200

    doesn't get more basic fuck a fadeout effect

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+# NoFadeout
+Removes some of the fadeout effects ingame
+
 ## Support/Contact me
 If you have any suggestions or need support, you can open a Github issue, or contact me [in my discord server.](https://discord.gg/AHcvPd9uru)
---
+++ src/main/java/com/bop/NoFadeout.java
@@ -0,0 +1,28 @@
+package com.bop;
+
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.Client;
+import net.runelite.api.events.ScriptPreFired;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+@Slf4j
+@PluginDescriptor(
+	name = "NoFadeout",
+	description = "Removes some of the fadeout effects ingame"
+)
+public class NoFadeout extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Subscribe
+	public void onScriptPreFired(ScriptPreFired event) {
+		if (event.getScriptId() == 948) {
+			event.getScriptEvent().getArguments()[4] = 255; // transparency
+			event.getScriptEvent().getArguments()[5] = 0; // "fadeout" duration
+		}
+	}
+}
